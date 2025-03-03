f5442fac59266ba0e37fca862ac40592b543f15a
Baidoc Razvan <baidocrazvan@gmail.com>
2025-03-03 04:54:17 +0200

    Add session, cookie and Passport.js authentication

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcryptjs";
 import env from "dotenv";
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
+
 
 const app = express();
 const port = 3001;
@@ -20,12 +24,28 @@ const db = new pg.Client({
 db.connect();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true})); // Middleware
+app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
   res.locals.year = new Date().getFullYear();
   next();
-}) // Middleware for creating local variable year to use inside the footer.
+}) // Middleware for creating local year variable to use inside the footer.
 
+// Session initialization
+app.use(
+  session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, // TODO: use express-session with postgress to store session
+  saveUninitialized: true, // save uninitialized session into the server memory
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // time until cookie expires, in miliseconds, totals 24hours
+    httpOnly: true,
+    sameSite: 'strict',
+  }
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get("/", (req, res) => {
@@ -43,43 +63,23 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-
-app.post("/api/login", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-    
-    if (result.rows.length > 0) {
-      const user = result.rows[0]
-      console.log(user);
-      const hashedPassword = user.password; // Hashed password during registration process
-
-      bcrypt.compare(password, hashedPassword, (err, result) => { // Compare user password against bcrypt hash from db
-        if (err) {
-          console.log("Error comparing passwords:", err);
-        } else {
-          if (result) {
-            res.send("Succesfully logged in!");
-          } else {
-            res.send("Incorrect password!");
-          }
-        }
-      })  
-    } else {
-      res.send("User not found. Try registering first.");
-    }
-  } catch (err) {
-    console.log(err);
+app.get("/loggedin", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("loggedin.ejs")
+  } else {
+    res.redirect("/login")
   }
-
 });
+
+app.post("/api/login", passport.authenticate("local", {
+  successRedirect: "/loggedin",
+  failureRedirect: "/login"
+}));
 
 app.post("/api/register", async (req, res) => {
   const lastName = req.body.lastname
   const firstName = req.body.firstname;
-  const email = req.body.email;
+  const email = req.body.username;
   const password = req.body.password;
   const confirmPassword = req.body.cpassword;
   const role = "user";
@@ -97,19 +97,23 @@ app.post("/api/register", async (req, res) => {
         const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
         if (checkResult.rows.length > 0) {
-          res.send("ERROR: Email already exists. Did you mean to log in?")
+          res.send("Email already exists. Did you mean to log in?")
         } else {
             // Hashing password using bcrypt
             bcrypt.hash(password, saltRounds, async (err, hash) => {
             if (err) {
               console.log("Error hashing password:", err);
             } else {
-              const result = await db.query("INSERT INTO users (first_name, last_name, email, password, role) VALUES ($1, $2, $3, $4, $5)", [
-              firstName, lastName, email, hash, role
+              const result = await db.query(
+                "INSERT INTO users (first_name, last_name, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *",[
+                firstName, lastName, email, hash, role
             ]);
-
-            console.log(result);
-            res.send("Succesfully registered!");
+            const user = result.rows[0];
+            console.log(user);
+            req.login(user, (err) => {
+              console.log(err)
+              res.redirect("/loggedin")
+            })
         }
       })
     }
@@ -121,9 +125,22 @@ app.post("/api/register", async (req, res) => {
   }
 });
 
-app.post("/api/logout", (req, res) => {
-  // Clear session cookie when user logs out
-}); 
+app.post("/api/logout", function(req, res, next) { // Clear session cookie when user logs out
+  req.logout((err) => { // Removes the req.user property and ends user's session
+    if (err) { return next(err); }
+
+    req.session.destroy(function(err) { // Destroy the session
+      if (err) { return next(err); }
+
+      res.clearCookie('connect.sid', {
+        httpOnly: true,
+        sameSite: "strict",
+      }); // Clear the session cookie
+
+      res.status(200).send('Logged out');
+    });
+  });
+});
 
 
 
@@ -181,6 +198,44 @@ app.get("/api/orders/:id", (req, res) => {
 
 }); // Get details of a specific order
 
+
+passport.use(new Strategy(async function verify(username, password, cb) {
+  
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+    
+    if (result.rows.length > 0) {
+      const user = result.rows[0]
+      console.log(user);
+      const hashedPassword = user.password; // Hashed password created during registration process
+
+      bcrypt.compare(password, hashedPassword, (err, result) => { // Compare user password against bcrypt hash from db
+        if (err) {
+          return cb(err)
+        } else {
+          if (result) {
+            return cb(null, user)
+          } else {
+            return cb(null, false)
+          }
+        }
+      })
+
+    } else {
+      return cb("User not found")
+    }
+  } catch (err) {
+    return cb(err);
+  }
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+})
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
