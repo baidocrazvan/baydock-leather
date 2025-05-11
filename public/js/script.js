6906afb254ad5bbc784ded6cd01fe4807e19ce6f
Baidoc Razvan <baidocrazvan@gmail.com>
2025-05-11 15:57:30 +0300

    fix(script): Fix event listener errors cause by admin-header

--- public/js/script.js
+++ public/js/script.js
@@ -31,31 +31,36 @@ window.addEventListener("scroll", () => {
 })
 
 // Toggle mobile menu and overlay
-hamburger.addEventListener("click", function (e) {
-    e.preventDefault();
-    navbar.classList.toggle("active");
-    overlay.classList.add("active");
-    // Toggle body scroll lock
-    // document.body.style.overflow = navbar.classList.contains('active') ? 'hidden' : '';
-    document.body.style.overflow = "hidden";
+if (hamburger) {
+  hamburger.addEventListener("click", function (e) {
+      e.preventDefault();
+      navbar.classList.toggle("active");
+      overlay.classList.add("active");
+      // Toggle body scroll lock
+      // document.body.style.overflow = navbar.classList.contains('active') ? 'hidden' : '';
+      document.body.style.overflow = "hidden";
 
-})
+  })
+}
 
 // Close mobile menu
-document.querySelector(".header__navbar-close").addEventListener("click", function() {
-    navbar.classList.remove("active");
-    document.querySelector(".header__navbar-overlay").classList.remove("active");
-    document.body.style.overflow = "";
-  });
+if (document.querySelector(".header__navbar-close")) {
+  document.querySelector(".header__navbar-close").addEventListener("click", function() {
+      navbar.classList.remove("active");
+      document.querySelector(".header__navbar-overlay").classList.remove("active");
+      document.body.style.overflow = "";
+    });
+}
 
-  // Close when clicking overlay
-overlay.addEventListener("click", function() {
+// Close when clicking overlay
+
+if (overlay) {
+  overlay.addEventListener("click", function() {
     navbar.classList.remove("active");
     overlay.classList.remove("active");
     document.body.style.overflow = "";
   });
-
-
+}
 
 
 // Search icon modal
@@ -65,23 +70,28 @@ const searchModal = document.querySelector(".header__search-modal");
 const searchClose = document.querySelector(".header__search-close");
 
 // Toggle modal
-searchIcon.addEventListener("click", () => {
-  searchModal.classList.add("header__search-modal--active");
-  document.querySelector(".header__search-input").focus();
-  document.body.style.overflow = "hidden";
-});
-
+if (searchIcon) {
+  searchIcon.addEventListener("click", () => {
+    searchModal.classList.add("header__search-modal--active");
+    document.querySelector(".header__search-input").focus();
+    document.body.style.overflow = "hidden";
+  });
+}
 // Close modal
-searchClose.addEventListener("click", () => {
-  searchModal.classList.remove("header__search-modal--active");
-});
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchModal.classList.remove("header__search-modal--active");
+  });
+}
 
 // Close when clicking outside
-searchModal.addEventListener("click", (e) => {
-  if (e.target === searchModal) {
-    searchModal.classList.remove("header__search-modal--active");
-  }
-});
+if (searchModal) {
+  searchModal.addEventListener("click", (e) => {
+    if (e.target === searchModal) {
+      searchModal.classList.remove("header__search-modal--active");
+    }
+  });
+}
 
 // Axios call for address delete
 async function deleteAddress(event, id) {
