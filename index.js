40cebceece37b3c1fffd2c76f97fbd1d07bd1827
Baidoc Razvan <baidocrazvan@gmail.com>
2025-06-09 16:07:29 +0300

    fix(server): Use PORT fron envinronment variables

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import addressRoutes from "./routes/addressRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const envFile =
   process.env.NODE_ENV === "test"
     ? ".env.test"
